678fec4cafe2cb9ead25735a97e43ef1ea5ca48a
anangfirmansyah <anangfirmansyahp5@gmail.com>
2024-02-01 22:08:53 +0700

    fix: pdf

--- app/api/getData/route.ts
+++ app/api/getData/route.ts
@@ -15,36 +15,6 @@ export async function POST(request: Request, res: Response) {
     const startDateTime = formatDatetime(start);
     const endDatetime = addOneDay(formatDatetime(to));
 
-    function formatTimestampToWIB(timestamp) {
-      // Buat objek Date dari timestamp
-      const date = new Date(timestamp);
-
-      // Ubah ke zona waktu WIB (UTC+7)
-      date.setHours(date.getHours() + 7);
-
-      // Format tanggal dan waktu menggunakan Intl.DateTimeFormat
-      const options = {
-        weekday: "long",
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
-        timeZoneName: "short",
-        timeZone: "Asia/Jakarta", // Zona waktu WIB
-      };
-
-      const formatter = new Intl.DateTimeFormat("id-ID", options);
-      const formattedDate = formatter.format(date);
-
-      return formattedDate;
-    }
-
-    const timestamp = "2024-01-31T13:03:55.10399+00:00";
-    const formattedWIB = formatTimestampToWIB(timestamp);
-    console.log(formattedWIB);
-
     const { data, error: err } = await supabase
       .from("pelanggan")
       .select("*")
--- app/rekap/components/form.tsx
+++ app/rekap/components/form.tsx
@@ -6,19 +6,43 @@ import axios from "axios";
 import { jsPDF } from "jspdf";
 import "jspdf-autotable";
 import { useState } from "react";
+import Swal from "sweetalert2";
 
 export default function Form() {
   const [data, setData] = useState<any[]>([]);
   const [startDate, setStartDate] = useState("");
   const [endDate, setEndDate] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const exportPdf = async () => {
     const doc = new jsPDF({ orientation: "portrait" });
 
-    // doc.text("Anang", 20, 30);
+    const fontSize = 12;
+    doc.setFontSize(fontSize);
+    const text = "SURVEY KEPUASAN PELAYANAN BPKB DITLANTAS POLDA BALI";
+
+    // Menghitung lebar teks
+    const textWidth = doc.getTextWidth(text);
+
+    // Menghitung posisi X agar teks berada di tengah
+    const pageWidth = doc.internal.pageSize.width;
+    const xPosition = (pageWidth - textWidth) / 2;
+
+    // Menambahkan teks di tengah dokumen
+    doc.text(text, xPosition, 20);
+
+    // @ts-ignore
+    doc.autoTable({
+      // html: "#my-table",
+      html: "#my-table-total",
+      startY: 30,
+    });
+
     // @ts-ignore
     doc.autoTable({
       html: "#my-table",
+      // html: "#my-table-total",
+      startY: 85,
     });
 
     doc.save("data.pdf");
@@ -27,21 +51,46 @@ export default function Form() {
   const handleFormSubmit = async (event: any) => {
     event.preventDefault();
 
-    if (new Date(startDate) > new Date(endDate)) {
-      console.log(
-        "Tanggal akhir harus lebih besar atau sama dengan tanggal awal."
-      );
-      return;
-    } else {
-      const { data } = await axios.post("http://localhost:3000/api/getData", {
-        start: startDate,
-        to: endDate,
+    try {
+      setIsLoading(true);
+
+      if (startDate === "" || endDate === "") {
+        Swal.fire({
+          title: "Gagal",
+          text: "Mohon untuk mengisi tanggal terlebih dahulu",
+          icon: "error",
+        });
+        return;
+      }
+
+      if (new Date(startDate) > new Date(endDate)) {
+        Swal.fire({
+          title: "Gagal",
+          text: "Tanggal akhir harus lebih besar atau sama dengan tanggal awal.",
+          icon: "error",
+        });
+        return;
+      } else {
+        const { data } = await axios.post("http://localhost:3000/api/getData", {
+          start: startDate,
+          to: endDate,
+        });
+
+        setData(data.data);
+        await new Promise((resolve) => setTimeout(resolve, 2));
+
+        exportPdf();
+      }
+    } catch (err) {
+      Swal.fire({
+        title: "Gagal",
+        text: "Rekap gagal, silahkan coba kembali",
+        icon: "error",
       });
-
-      setData(data.data);
-      await new Promise((resolve) => setTimeout(resolve, 2));
-
-      exportPdf();
+    } finally {
+      // setStartDate("");
+      // setEndDate("");
+      setIsLoading(false);
     }
   };
 
@@ -65,6 +114,7 @@ export default function Form() {
                   <input
                     type="date"
                     // onChange={handleStartDateChange}
+                    value={startDate}
                     onChange={(e) => setStartDate(e.target.value)}
                     className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
                   />
@@ -76,19 +126,60 @@ export default function Form() {
                   </label>
                   <input
                     type="date"
+                    value={endDate}
                     onChange={(e) => setEndDate(e.target.value)}
                     // onChange={handleEndDateChange}
                     className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
                   />
                 </div>
               </div>
-              <button className="flex w-full justify-center rounded bg-primary p-3 font-medium text-gray">
-                Buat Rekap
+              <button
+                disabled={isLoading}
+                className={`flex w-full justify-center rounded  p-3 font-medium  ${
+                  isLoading ? "bg-gray text-black" : "bg-primary text-gray"
+                }`}>
+                {isLoading ? "Loading" : "Buat Rekap"}
               </button>
             </div>
           </form>
         </div>
       </div>
+
+      <table id="my-table-total" className="hidden">
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>Kepuasan</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Sangat Puas</td>
+            <td>{data.filter((item) => item.kepuasan === "3").length}</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Puas</td>
+            <td>{data.filter((item) => item.kepuasan === "2").length}</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Cukup</td>
+            <td>{data.filter((item) => item.kepuasan === "1").length}</td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>Tidak Puas</td>
+            <td>{data.filter((item) => item.kepuasan === "0").length}</td>
+          </tr>
+          <tr>
+            <td colSpan={2}>Total</td>
+            <td>{data.length}</td>
+          </tr>
+        </tbody>
+      </table>
 
       <table id="my-table" className="hidden">
         <thead>
